62ea6c7e063bdc331ac4c682a3fcf45fb4230743
Egemen Öztin <xaron.egemen.oztin@gmail.com>
2025-01-30 14:40:21 +0100

    Create utils for temperature unit type conversion

---
+++ WeatherApp.API/Utils.cs
@@ -0,0 +1,26 @@
+﻿using WeatherApp.API.Models;
+
+namespace WeatherApp.API
+{
+  public static class Utils
+  {
+    public static TempratureUnitType ConvertTempratureUnitType(string unit)
+    {
+      return unit.ToLower() switch
+      {
+        "metric" => TempratureUnitType.METRIC,
+        "imperial" => TempratureUnitType.IMPERIAL,
+        _ => throw new ArgumentException($"Invalid unit: {unit}")
+      };
+    }
+    public static string ConvertTempratureUnitToString(TempratureUnitType unit)
+    {
+      return unit switch
+      {
+        TempratureUnitType.METRIC => "metric",
+        TempratureUnitType.IMPERIAL => "imperial",
+        _ => throw new ArgumentException($"Invalid unit: {unit}")
+      };
+    }
+  }
+}
